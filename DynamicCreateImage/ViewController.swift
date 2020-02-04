44bd716391276e8c5fefe907579118ac5fbbc7e1
zino <minzino@hanmail.net>
2020-02-04 10:58:33 +0900

    last commit

--- DynamicCreateImage/ViewController.swift
+++ DynamicCreateImage/ViewController.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var y:Int = 100
+    var num:Int = 1
+    
+    @IBAction func btnClick(_ sender: Any) {
+        createImage()
+    }
+    
+    func createImage(){
+        
+        let img = UIImage(named: String(format: "h%d.png", num))
+        let imgView = UIImageView(image: img)
+        imgView.frame = CGRect(x: 100, y: y, width: 50 , height: 50)
+        self.view.addSubview(imgView)
+        
+        y += 60
+        num += 1
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
